25beacc9c4530f29dd5bed95c247eb883d085ad9
Денис Дриц <rezerv50@yandex.ru>
2023-11-09 21:34:45 +0300

    Change settings

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,19 +24,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "-0ib2b@mm@7jwi!d_ler4&j%qh5j4a%w_o_@dl)as1!zts6+qq"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "[::1]",
-    "testserver",
-    "www.Den2605.pythonanywhere.com",
-    "Den2605.pythonanywhere.com",
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+
 
 EMPTY_VALUE_DISPLAY = "-пусто-"
 
